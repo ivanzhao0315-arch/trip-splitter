63571e81debab4e65aa4a9ac47f26e7aef8926c6
ivanzhao0315-arch <ivan.zhao0315@gmail.com>
2026-06-15 19:07:59 +0400

    Allow relative static builds

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/trip-splitter/' : '/',
+  base: process.env.RELATIVE_BASE === 'true'
+    ? './'
+    : process.env.GITHUB_PAGES === 'true'
+      ? '/trip-splitter/'
+      : '/',
 });
